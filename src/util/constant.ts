4e93dbe5ba13e47cf3b866ad719d31b376cb7247
Hardik Patel <mailtohardikpatel@gmail.com>
2025-01-13 19:38:16 +0530

    feat: enhancements and minor fixes

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -1,4 +1,4 @@
-export const DELIMETER_SEARCH_VALUE: RegExp = /[.*+?^${}()|[\]\\]/g;
+export const DELIMETER_SEARCH_VALUE: RegExp = /[%.*+?^${}()|[\]\\]/g;
 export const DELIMETER_REPLACE_VALUE: string = "\\$&";
 export const CUSTOM_DELIMETERS_MATCH: RegExp = /^\/\/(.*?)\n/; 
 export const EXTRACT_DELIMETERS: RegExp = /\[([^\]]+)\]/g;
