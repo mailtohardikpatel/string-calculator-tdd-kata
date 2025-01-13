14898e431b6e55b2b8777954d7b1a7ac3788b7f4
Hardik Patel <mailtohardikpatel@gmail.com>
2025-01-13 17:14:50 +0530

    feat: lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
